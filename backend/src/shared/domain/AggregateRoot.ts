61745acc5818a394c958234526fbdc99a663dd26
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-02-07 04:17:00 -0600

    AggregateRoot abstract class now supports domain events

--- backend/src/shared/domain/AggregateRoot.ts
+++ backend/src/shared/domain/AggregateRoot.ts
@@ -1,3 +1,26 @@
 import { Entity } from './Entity';
+import { DomainEvents } from './events/DomainEvents';
+import { IDomainEvent } from './events/IDomainEvent';
+import { UniqueEntityId } from './UniqueEntityId';
 
-export abstract class AggregateRoot<T> extends Entity<T> {}
+export abstract class AggregateRoot<T> extends Entity<T> {
+    private _domainEvents: IDomainEvent[] = [];
+
+    get id(): UniqueEntityId {
+        return this._id;
+    }
+
+    get domainEvents(): IDomainEvent[] {
+        return this._domainEvents;
+    }
+
+    protected addDomainEvent(domainEvent: IDomainEvent): void {
+        this.domainEvents.push(domainEvent);
+
+        DomainEvents.markAggregateForDispatch(this);
+    }
+
+    public clearEvents(): void {
+        this._domainEvents.splice(0, this._domainEvents.length);
+    }
+}
