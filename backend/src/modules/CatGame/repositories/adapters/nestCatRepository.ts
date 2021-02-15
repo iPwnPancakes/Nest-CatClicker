098862a0fa635bfa1fe2ca951ba3d650a7350867
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-02-14 22:46:15 -0600

    Implemented catRepository
    Added Cat <-> Room relationship in repo
    
    Removed Cat <-> Owner relationship in repo

---
+++ backend/src/modules/CatGame/repositories/adapters/nestCatRepository.ts
@@ -0,0 +1,68 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Cat } from '../../../../shared/infrastructure/framework/nestjs/typeorm/models/Cat.entity';
+import { Cat as CatDomainModel } from '../../domain/Cat';
+import { CatMapper } from '../../mappers/catMap';
+import { ICatRepository } from '../ports/catRepository';
+
+export class NestCatRepository implements ICatRepository {
+    constructor(
+        @InjectRepository(Cat)
+        private catRepo: Repository<Cat>,
+    ) {}
+
+    async exists(catId: string): Promise<boolean> {
+        const cat = await this.catRepo.findOne(catId);
+
+        if (!cat) {
+            return false;
+        }
+
+        return true;
+    }
+
+    async getCatByCatId(catId: string): Promise<CatDomainModel> {
+        const cat = await this.catRepo.findOne(catId);
+
+        if (!cat) {
+            throw new Error('Cat not found');
+        }
+
+        return CatMapper.toDomain(cat);
+    }
+
+    async getCatsByRoomId(roomId: string): Promise<CatDomainModel[]> {
+        const cats = await this.catRepo.find({
+            relations: ['room'],
+            where: {
+                room: {
+                    id: roomId,
+                },
+            },
+        });
+
+        return cats.map(cat => CatMapper.toDomain(cat));
+    }
+
+    async save(cat: CatDomainModel): Promise<void> {
+        const exists = this.exists(cat.catId.id.toString());
+
+        const catOrmModel = await CatMapper.toPersistence(cat);
+
+        if (!exists) {
+            await this.catRepo.save(catOrmModel);
+        } else {
+            await this.catRepo.update(cat.catId.id.toString(), cat);
+        }
+    }
+
+    async delete(cat: CatDomainModel): Promise<void> {
+        await this.catRepo.delete(cat.catId.id.toString());
+    }
+
+    async saveBulk(cats: CatDomainModel[]): Promise<void> {
+        for (const cat of cats) {
+            await this.save(cat);
+        }
+    }
+}
--- backend/src/modules/CatGame/repositories/ports/catRepository.ts
+++ backend/src/modules/CatGame/repositories/ports/catRepository.ts
@@ -3,8 +3,7 @@ import { Cat } from '../../domain/Cat';
 export interface ICatRepository {
     exists(catId: string): Promise<boolean>;
     getCatByCatId(catId: string): Promise<Cat>;
-    getCatsByLevel(levelId: string): Promise<Cat[]>;
-    getCatsByOwnerId(ownerId: string): Promise<Cat[]>;
+    getCatsByRoomId(roomId: string): Promise<Cat[]>;
     save(cat: Cat): Promise<void>;
     delete(cat: Cat): Promise<void>;
     saveBulk(cats: Cat[]): Promise<void>;
