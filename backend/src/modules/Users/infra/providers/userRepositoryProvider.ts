1c4f7e33cd3965150d0ab76f091b6dfdeacedf82
iPwnPancakes <Daniel@DanielBuenrrostro.com>
2021-04-04 23:44:40 -0500

    UserRepo provider is now typed as a provider

--- backend/src/modules/Users/infra/providers/userRepositoryProvider.ts
+++ backend/src/modules/Users/infra/providers/userRepositoryProvider.ts
@@ -1,7 +1,8 @@
 import { IUserRepository } from '../../repositories/ports/userRepository';
 import { NestUserRepository } from '../../repositories/adapters/nestUserRepository';
+import { Provider } from '@nestjs/common';
 
-export const UserRepositoryProvider = {
+export const UserRepositoryProvider: Provider = {
     provide: IUserRepository,
     useClass: NestUserRepository,
 };
